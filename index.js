270f35327ce14bc68d7aa6fed8734ed8492f9bc8
Rushikesh7997 <pardhikar26@gmail.com>
2022-11-07 09:47:29 +0530

    about page added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/",(req,res)=>{
     res.send("hello rishi, this is check of deploymet.")
 })
 
+app.get("/about",(req,res)=>{
+    res.send("this is about page")
+})
+
 app.listen(PORT,()=>{
     console.log(`app is running on ${PORT}`)
 })
